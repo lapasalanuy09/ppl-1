5810bfdc13364957083a25a6d7a9f96e307d33d9
nandanrhmnt <nandanurahmanita@gmail.com>
2023-12-18 23:44:46 +0700

    Merge changes

--- app/Http/Controllers/DetailKegiatanController.php
+++ app/Http/Controllers/DetailKegiatanController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Data_kegiatan;
 use Illuminate\Http\Request;
 
 class DetailkegiatanController extends Controller
 {
     public function index()
     {
-        return view('detail_kegiatan.index');
+        return view('detail_kegiatan.index', [
+            "kegiatans" => Data_kegiatan::all()
+        ]);
     }
 }
